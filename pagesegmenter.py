5e3f4544e301f31463e024e3e4cc19ccedb74a47
Avadesh <avadesh02@gmail.com>
2017-02-05 15:46:22 +0530

    "first commit, solution to exception letters"

---
+++ pagesegmenter.py
@@ -0,0 +1,253 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+class word_finder:
+    
+	def __init__(self,img):
+		self.img = cv2.imread(img,0)
+		self.img_color = cv2.imread(img)
+		self.img = cv2.resize(self.img,(0,0), fx=0.75, fy=0.75)
+		self.img_color = cv2.resize(self.img_color,(0,0), fx=0.75, fy=0.75)
+		self.cimg = cv2.Canny(self.img,200,250)
+		#(t,self.cimg) = cv2.threshold(self.img,160,255,cv2.THRESH_BINARY)
+		self.rows = self.img.shape[0]
+		self.cols = self.img.shape[1]
+
+
+
+	def find_words(self,l_limit,r_limit):
+		print('obtaining words....')
+		line_count_matrix = []  #identifies number of white pixels in line
+		line_matrix = []  #contains co-ordinates to draw boxes around lines
+		#print("obtaining line_count")
+
+		for y in range(0,self.rows -1):
+			count = 0
+			for x in range(l_limit,r_limit):
+				if self.cimg[y][x] == 255:
+					count += 1
+			line_count_matrix.append(count)
+		#print(" obtained line_count") 		
+		
+		#print("obtaining lines")
+		y = 0
+		while y < len(line_count_matrix) - 1 :
+			if line_count_matrix[y] > 30:
+				line_matrix.append(y)
+				for y2 in range(y + 3, len(line_count_matrix) - 1):
+					if line_count_matrix[y2] < 8:
+						line_matrix.append(y2)
+						y = y2
+						break
+			y +=1
+		#print(len(line_matrix))
+		#print("obtained lines")	
+		#print("obtaining count for words")
+		self.line_matrix = line_matrix
+		count = 0
+		x_count_matrix = []
+		word_matrix = []
+		for i in range(0,len(line_matrix)- 1,2):	
+			x_count_matrix.append([])
+			for x in range(l_limit,r_limit):
+				count = 0
+				for y in range(line_matrix[i],line_matrix[i+1]):
+					if self.cimg[y][x] == 255:
+						count += 1			
+				x_count_matrix[i/2].append(count)
+		#print("count for words has been obtained")		
+		self.x_count_matrix = x_count_matrix	
+		#print(len(x_count_matrix[0]))
+		for y in range(0,len(x_count_matrix)-1):
+			word_matrix.append([])
+			#word_matrix[y].append(l_limit)
+			x = 0
+			while x < len(x_count_matrix[y]) - 5:
+				if x_count_matrix[y][x] + x_count_matrix[y][x+1] + x_count_matrix[y][x+2] + x_count_matrix[y][x+3] < 5:
+					word_matrix[y].append(x + l_limit)
+					for x2 in range(x+4,len(x_count_matrix[y])-5):
+						if x_count_matrix[y][x2] > 1:
+							word_matrix[y].append(x2 + l_limit)
+						 	x = x2
+							break
+				x += 4
+			word_matrix[y].append(r_limit)
+		self.word_matrix = word_matrix 	
+		word_array = []
+		
+		for y in range(0,len(self.line_matrix)-2,2):
+			for x in range(1,len(self.word_matrix[y/2])-2,2):
+				if self.word_matrix[y/2][x+1] - self.word_matrix[y/2][x] > 4: #change 4 to a smaller number to include punctuations.
+					word_array.append(self.word_matrix[y/2][x] - 3)
+					word_array.append(self.line_matrix[y]-5)
+					word_array.append(self.word_matrix[y/2][x+1] + 3)
+					word_array.append(self.line_matrix[y+1]+2)
+		self.word_array = word_array   #ultimate array to find words, formate (x1,y1,x2,y2) top left and bottom right corner of word
+		#print("word array has been created")				
+		print("words obtained")
+	
+	def show_lines(self,l_limit,r_limit):
+		for y in range(0,len(self.line_matrix) -2,2):
+			cv2.rectangle(self.img,(l_limit,self.line_matrix[y] - 5),(r_limit,self.line_matrix[y+1]),0,1)
+		cv2.imshow('Image',self.img)
+		cv2.waitKey(0)
+	
+	def show_words(self):
+		print("drawing words")
+		for i in range(0,len(self.word_array)-1,4):
+			cv2.rectangle(self.img_color,(self.word_array[i],self.word_array[i+1]),(self.word_array[i+2],self.word_array[i+3]),0,1)
+
+	def show_image(self):	
+		cv2.imshow('Image',self.img_color)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+	def store_words(self,no_words):
+		print('storing words.....')
+		for i in range(0,len(self.word_array),4):
+			cropped = self.img_color[self.word_array[i+1]:self.word_array[i+3],self.word_array[i]:self.word_array[i+2]]
+			cv2.imwrite('words/'+str(no_words + i/4)+'.png',cropped)
+		print('words have been stored ......')
+
+################################ HANDLER FUNCTIONS FOR WORDS ##########################################
+
+	def segment_page_into_words(self):
+		self.find_words(20,self.cols/2)
+		self.store_words(0)
+		no_words = len(os.listdir('./words'))		
+		self.find_words(self.cols/2,self.cols)
+		self.store_words(no_words)		
+#############################################################################################
+class letter_finder:
+	def __init__(self,img):
+		self.img = cv2.imread(img)
+		self.img = cv2.resize(self.img,(0,0),fx=10,fy=5)
+		avg = np.average(self.img)
+		med = np.median(self.img)
+		self.b_img = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
+		thr_val = (avg) - 40
+		(t,thr_img) = cv2.threshold(self.b_img,thr_val,255,cv2.THRESH_BINARY)
+		self.thr_img = thr_img 
+		self.rows = self.img.shape[0]
+		self.cols = self.img.shape[1]
+
+	def find_line(self):
+		count_matrix = []
+		for y in range(0,self.thr_img.shape[0]-1):
+			count = 0
+			for x in range(0,self.thr_img.shape[1]-1):
+				if self.thr_img[y][x] == 0:
+					count += 1
+			count_matrix.append(count)
+		for i in range(len(count_matrix)-2,0,-1):
+			if count_matrix[i] > int(self.cols/5) :
+				bottom_line = i
+				break 
+		return (count_matrix,bottom_line)
+
+	def remove_line(self,count_matrix):
+		margin = 11
+		y_line = count_matrix[0].index(max(count_matrix[0]))
+		upper_img = self.thr_img[0:(y_line - margin),0:self.cols]
+		lower_img = self.thr_img[(y_line + margin):count_matrix[1],0:self.cols]
+		
+		final_image = np.concatenate((upper_img,lower_img),axis=0)
+		#print('image has been formed without line')
+		self.final_image = final_image
+
+	def show_letters(self):
+		for i in range(0,len(self.letter_matrix)-2):
+			cv2.rectangle(self.img,(self.letter_matrix[i],0),(self.letter_matrix[i+1],self.cols),0,2)
+		#print('letters are drawn')		
+
+	def count_region(self,count_matrix,pos,r):
+		count = sum(count_matrix[pos-r:pos+r])
+		return count
+
+	def find_letters(self,x1,y1,x2,y2):
+		count_matrix = []
+		letter_matrix = []
+		letter_matrix.append(x1 + 10)
+		for x in range(x1,x2):
+			count = 0
+			for y in range(y1,y2):
+				if self.final_image[y][x] == 0:
+					count += 1	
+			count_matrix.append(count)
+		#print(count_matrix)
+		x = x1 + 80
+		while x < len(count_matrix)-2:
+			if self.count_region(count_matrix,x,3) < 2:
+				if (x + x1) not in letter_matrix:
+					letter_matrix.append(x + x1 + 10)
+					x += 40
+			x += 1
+		#to correct errors like gha, sha, na
+		#print('len',len(letter_matrix))
+		for first,second in zip(letter_matrix,letter_matrix[1:]):
+			if second - first < 55:
+				letter_matrix.remove(first)
+		#print('letters have been found')
+		#print(letter_matrix)
+		self.letter_matrix = letter_matrix
+		self.no_words = len(letter_matrix)-1
+		self.count_matrix = count_matrix
+
+	def show_letters(self):#draws boxes around letters
+		for i in range(0,len(self.letter_matrix)-1):
+			cv2.rectangle(self.img,(self.letter_matrix[i],0),(self.letter_matrix[i+1],self.rows),0,1)
+		#print('letters are drawn')	
+
+	def resize_image(self,x,y):
+		self.img = cv2.resize(self.img,(0,0),fx=x,fy=y)
+
+	def show_image(self):
+		cv2.imshow('letters',self.img)
+		cv2.waitKey(0)
+
+	def show_cropped_image(self):
+		cv2.imshow('letters',self.final_image)
+		cv2.waitKey(0)
+
+	def plot_intensity(self):
+		x = []
+		for i in range(len(self.count_matrix)):
+			x.append(i)
+		
+		plt.plot(x,self.count_matrix)
+		plt.show()	
+	
+	def crop_letters(self,word_index):
+		for x in range(0,len(self.letter_matrix)-1):
+			letter = self.img[0:self.rows , self.letter_matrix[x]:self.letter_matrix[x+1]]
+			letter = cv2.resize(letter,(0,0),fx=0.2,fy=0.2)
+			cv2.imwrite('letters/' +str(word_index) + str(x)+'.png',letter)
+		
+		#print('letters have been stored...')	
+
+	def store_cropped_letters(self,word_index):
+		y = self.find_line()
+		self.remove_line(y)
+		self.find_letters(0,0,self.cols,self.final_image.shape[0])
+		self.crop_letters(word_index)
+		#self.show_letters()
+		#self.show_image()
+##################################test functions below###########################
+
+#img = 'test/20.png'
+#image = letter_finder(img)
+#image.store_cropped_letters(0)
+#y = image.find_line()
+#image.remove_line(y)
+#image.find_letters(0,0,image.cols,image.final_image.shape[0])
+#image.show_letters()
+#image.show_image()
+#image.crop_letters()
+
+#img = '1996January/19.jpg' 
+#image = word_finder(img)
+#image.segment_page_into_words()
+
+
---
+++ run.py
@@ -0,0 +1,177 @@
+from flask import *
+from pagesegmenter import *
+import cv2
+import numpy as np
+from flask import *
+import os
+import sys
+from flask import session
+import cPickle as pickle
+import shutil
+
+app = Flask(__name__,static_url_path = "/words", template_folder='templates')
+app.config['SECRET_KEY'] = 'oh_so_secret'
+
+
+
+def store_f(start,no_words):
+	no_letter_array = [] #stores number of letters in each word
+	for i in range(start,no_words):
+		img = 'words/'+ str(i) + '.png'
+		word = letter_finder(img)
+		word.store_cropped_letters(i)
+		no_letter_array.append(word.no_words)
+	return(no_letter_array)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/image/get',methods=['POST','GET'])
+def get_image():
+	current_dir = os.getcwd()
+	final_dir = os.path.join(current_dir, r'page')
+	if os.path.exists(final_dir):
+		shutil.rmtree(final_dir)
+		os.makedirs(final_dir)	
+	else:
+		os.makedirs(final_dir)	
+	file = request.files['page']
+	file.save('page/image.jpg')
+	return redirect(url_for('segment_image'))
+
+@app.route('/image/process')
+def segment_image():
+	img = 'page/image.jpg'
+	current_dir = os.getcwd()
+	final_dir = os.path.join(current_dir, r'words')
+	if os.path.exists(final_dir):
+		shutil.rmtree(final_dir)
+		os.makedirs(final_dir)	
+	else:
+		os.makedirs(final_dir)	
+		
+	image = word_finder(img)
+	image.segment_page_into_words()	
+
+	return redirect(url_for('segment_words'))	
+
+@app.route('/words/process')
+def segment_words():
+	current_dir = os.getcwd()
+	final_dir = os.path.join(current_dir, r'letters')
+	if os.path.exists(final_dir):
+		shutil.rmtree(final_dir)
+		os.makedirs(final_dir)	
+	else:
+		os.makedirs(final_dir)	
+	word_array = os.listdir('./words')
+	print('finding letters and storing.....')
+	label_array = store_f(0,len(word_array)-2)
+	session['label_array'] = label_array
+	print('letters have been stored...')
+	print('uploading letters....')
+	session['start'] = 0
+	session['flag'] = 0
+	session['no_words'] = session['start'] + 25
+	return redirect(url_for('dev_home'))	
+
+@app.route('/get/nextset',methods=['POST','GET'])
+def get_next_set():
+	value = 25
+	limit = int(len(os.listdir('./words')))	
+#	value = request.form['value']
+#	print(value)
+	if limit -session['start'] < 30 and limit -session['start'] > 0:
+		print('if statment worked')
+		print(session['flag'])
+		session['start'] += 25
+		session['no_words'] = limit-1 	
+	elif limit - session['start'] > 25:
+		print('elif is working')
+		session['start'] += 25
+		session['no_words'] = session['start'] + 25
+	return redirect(url_for('dev_home'))
+
+@app.route('/get/lastset',methods=['POST','GET'])
+def go_back_one_set():
+	
+	value = request.form['value']
+#	print(value)
+	if session['start'] > 0:
+		session['start'] -=  25
+		session['no_words']  = session['start'] + 25
+	
+	return redirect(url_for('dev_home'))
+
+@app.route('/dev_home')
+def dev_home():
+		return render_template('dev_page.html',no_words = session['no_words'],no_letter_array = session['label_array'],start = session['start'])	#start is th point from which words are thrown 
+
+
+@app.route('/get/label_data',methods=['POST','GET'])
+def get_data():
+	temp_label_array = request.form.getlist('label')
+	no_letter_array = session['label_array']
+	#print(session['tmp'])
+	index = 0
+	label_array = []
+	for w in range(session['start'],session['no_words']):
+		label_array.append([])
+		for l in range(0,no_letter_array[w]):
+			label_array[w-session['start']].append(temp_label_array[index])
+			index += 1
+	#print(label_array)
+	#print(no_letter_array)
+	session['target_array'] = label_array
+	session['no_letter_array'] = no_letter_array 
+	return render_template('temp_page.html',temp_label_array=temp_label_array,no_letter_array = no_letter_array,no_words = session['no_words'],label_array = label_array,start = session['start'])
+
+@app.route('/get/confirmation',methods=['GET','POST'])
+def get_confirmation():
+	dataset = []
+	target_array = session['target_array']
+	index = 0
+	for w in range(session['start'],session['no_words']):
+		for l in range(0,session['no_letter_array'][w]):
+			img = 'letters/' + str(w) + str(l) + '.png' 
+			letter_image = cv2.imread(img)
+			dataset.append([])
+			dataset[index].append(letter_image)
+			dataset[index].append(target_array[w-session['start']][l])	
+			index += 1
+
+	current_dir = os.getcwd()
+	final_dir = os.path.join(current_dir, r'database')
+	if not os.path.exists(final_dir):
+		os.makedirs(final_dir)
+	i = os.listdir('./database')
+	print(i)
+	name_new_database = final_dir + '/' + str(len(i)) + '.p'
+		
+	pickle.dump( dataset ,open(name_new_database,'wb'))
+	return redirect(url_for('get_next_set'))
+
+
+@app.route('/uploads/<filename>')
+def upload_image(filename):
+	return send_from_directory('words',filename)
+
+@app.route('/upload/<filename>')
+def upload_letters(filename):
+	return send_from_directory('letters',filename)
+
+@app.route('/uploaded/<filename>')
+def upload_page(filename):
+	return send_from_directory('page',filename)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
